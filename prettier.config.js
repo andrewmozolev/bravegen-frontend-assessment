74a04981a520d6117aa6d0b09843a5f9426111f8
Andrew Mozolev <andrew.mozolev@gmail.com>
2025-03-19 22:15:57 +1300

    Add Prettier and Tailwind CSS plugin for code formatting

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  trailingComma: 'es5',
+  tailwindFunctions: ['clsx', 'tw'],
+  plugins: ['prettier-plugin-tailwindcss'],
+}
