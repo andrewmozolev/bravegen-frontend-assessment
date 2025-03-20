77d1ec85fc2c4dc388402cfc7a54e740787a3150
Andrew Mozolev <andrew.mozolev@gmail.com>
2025-03-20 23:39:22 +1300

    Wrap App component with QueryClientProvider and ModalProvider for improved state management and UI context

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import ModalProvider from './components/Modal/Modal.tsx';
 import './index.css';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
